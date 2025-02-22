ce911cc548e82d25b9150284410b14cd344aa153
KarthikeyanJothi <Karthikeyan.Jothiramalingam@LTIMindtree.com>
2025-02-23 02:36:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import requests
 st.title(" :cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
-    """
+    """	
 )
 
 import streamlit as st
