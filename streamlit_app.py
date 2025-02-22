eb17eb1aeedf94fa78f876a7f4f61f909c072dca
KarthikeyanJothi <Karthikeyan.Jothiramalingam@LTIMindtree.com>
2025-02-23 00:28:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,18 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-	smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-	sf_df = st.dataframe(data=smoothiefroot_respose.json(), use_container_width=True)
+	    for fruit_chosen in ingredients_list:
+	        ingredients_string += fruit_chosen + ' '
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+		sf_df = st.dataframe(data=smoothiefroot_respose.json(), use_container_width=True)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-    #st.write(my_insert_stmt)
-    #st.stop()
+		    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+		                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+		    #st.write(my_insert_stmt)
+		    #st.stop()
     
     
-    time_to_insert = st.button('Submit Order')
+		    time_to_insert = st.button('Submit Order')
     
     
     if time_to_insert:
